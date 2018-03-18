a25c0208dd351f9a5de73ff9a2ab44cd0a4a3091
zhangGPeng <576235325@qq.com>
2018-03-18 15:59:50 +0800

    第一次提交

---
+++ console.js
@@ -0,0 +1,20 @@
+console.info("程序开始执行：");
+
+var counter = 10;
+console.log("计数: %d", counter);
+
+console.time("获取数据");
+//
+// 执行一些代码
+// 
+console.timeEnd('获取数据');
+
+// 输出当前目录
+console.log('当前目录: ' + process.cwd());
+
+// 输出当前版本
+console.log('当前版本: ' + process.version);
+
+// 输出内存使用情况
+console.log(process.memoryUsage());
+console.info("程序执行完毕。");
